eb5e9ba25b6d592168db6709ae7532d3a57252c6
Diego Ortiz <diegoortizmatajira@gmail.com>
2021-03-21 17:20:33 -0500

    Zsh

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,13 +8,11 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
 for m in $(polybar --list-monitors | grep "(primary)" | cut -d":" -f1); do
-    MONITOR=$m polybar primary-bar &
-    # MONITOR=$m polybar --reload basic-bar &
+    MONITOR=$m polybar --reload primary-bar &
 done
 
 for m in $(polybar --list-monitors | grep -v "(primary)" | cut -d":" -f1); do
-    MONITOR=$m polybar basic-bar &
-    # MONITOR=$m polybar --reload basic-bar &
+    MONITOR=$m polybar --reload basic-bar &
 done
 
 echo "Polybar launched..."
