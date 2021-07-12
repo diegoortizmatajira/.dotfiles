da4d591e7a0f04422236b31ac7606845cd7734ce
Diego Ortiz <diegoortizmatajira@gmail.com>
2021-07-12 09:06:32 -0500

    Autorandr settings

--- local-bin/layout-partially-docked-2.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP-1 --mode 1366x768 --pos 1600x0 --rotate normal --output DP-1 --primary --mode 1600x900 --pos 0x0 --rotate normal --output HDMI-1 --off --output HDMI-2 --off
--- local-bin/layout-tv.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP-1 --primary --mode 1366x768 --pos 1643x1080 --rotate normal --output DP-1 --off --output HDMI-1 --off --output HDMI-2 --mode 1920x1080 --pos 1366x0 --rotate normal
