82b880fcc97e4ff95d2b9d3138c832a90c33c595
Diego Ortiz <diegoortizmatajira@gmail.com>
2021-11-15 09:28:33 -0500

    layout displays

--- local-bin/custom-layout.sh
+++ local-bin/custom-layout.sh
@@ -5,23 +5,26 @@ COMMANDS=()
 TITLES[0]="Auto-Detect"
 COMMANDS[0]="autorandr"
 
-TITLES[1]="Fully Docked (HDMI+VGA+Laptop)"
-COMMANDS[1]="autorandr FullyDocked"
+TITLES[1]="0. Undocked (Only Laptop)"
+COMMANDS[1]="autorandr UnDocked"
 
-TITLES[2]="Undocked (Only Laptop)"
-COMMANDS[2]="autorandr UnDocked"
+TITLES[2]="1. External Displays Only"
+COMMANDS[2]="autorandr ExternalDisplaysOnly"
 
-TITLES[3]="Partially Docked (HDMI+Laptop)"
-COMMANDS[3]="autorandr PartiallyDockedHDMI"
+TITLES[3]="2. External Display HDMI Only"
+COMMANDS[3]="autorandr ExternalDisplayHDMI"
 
-TITLES[4]="Partially Docked (VGA+Laptop)"
-COMMANDS[4]="autorandr PartiallyDocked"
+TITLES[4]="3. External Display VGA Only"
+COMMANDS[4]="autorandr ExternalDisplayVGA"
 
-TITLES[5]="TV Plugged (TV+Laptop)"
-COMMANDS[5]="autorandr TV"
+TITLES[5]="4. Fully Docked (HDMI+VGA+Laptop)"
+COMMANDS[5]="autorandr FullyDocked"
 
-TITLES[6]="Manual setup"
-COMMANDS[6]="arandr"
+TITLES[6]="5. TV Plugged (TV+Laptop)"
+COMMANDS[6]="autorandr TV"
+
+TITLES[7]="9. Manual setup"
+COMMANDS[7]="arandr"
 
 function getOptions() {
     for item in "${TITLES[@]}"; do
