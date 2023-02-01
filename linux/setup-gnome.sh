617eb1518d922cbd28957f37e3bb52bc0ae4b5fc
Diego Ortiz <diegoortizmatajira@gmail.com>
2023-02-01 11:21:04 -0500

    Add numpad keys to gnome

--- linux/setup-gnome.sh
+++ linux/setup-gnome.sh
@@ -10,7 +10,7 @@ for i in {1..10}
 do
   NORMALIZED_I=$(($i%10))
   # Bind Super + n to switch to a workspace
-  gsettings set "org.gnome.desktop.wm.keybindings" "switch-to-workspace-$i" "['<Super>$NORMALIZED_I']"
+  gsettings set "org.gnome.desktop.wm.keybindings" "switch-to-workspace-$i" "['<Super>$NORMALIZED_I', '<Super>KP_$NORMALIZED_I']"
   # Bind Super + n to move a window to a workspace
-  gsettings set "org.gnome.desktop.wm.keybindings" "move-to-workspace-$i" "['<Super><Shift>$NORMALIZED_I']"
+  gsettings set "org.gnome.desktop.wm.keybindings" "move-to-workspace-$i" "['<Super><Shift>$NORMALIZED_I', '<Super><Shift>KP_$NORMALIZED_I']"
 done
