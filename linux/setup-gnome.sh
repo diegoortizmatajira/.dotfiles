36ce93c0d30db44946f343405b0852deb2d72877
Diego Ortiz <diegoortizmatajira@gmail.com>
2023-02-27 12:10:07 -0500

    Gnome setup

--- linux/setup-gnome.sh
+++ linux/setup-gnome.sh
@@ -14,8 +14,16 @@ do
   # Bind Super + n to move a window to a workspace
   gsettings set "org.gnome.desktop.wm.keybindings" "move-to-workspace-$i" "['<Super><Shift>$NORMALIZED_I', '<Super><Shift>KP_$NORMALIZED_I']"
 done
-
-gsettings set "org.gnome.desktop.wm.keybindings" "toggle-maximized" "['<Super>m', '<Alt>F10']"
+gsettings set org.gnome.desktop.interface clock-format '12h'
+gsettings set org.gnome.desktop.wm.preferences button-layout "close,minimize,maximize:appmenu"
+gsettings set org.gnome.desktop.wm.keybindings toggle-fullscreen "['<Super>f']"
+gsettings set org.gnome.desktop.wm.keybindings toggle-maximized "['<Super>m', '<Alt>F10']"
+gsettings set org.gnome.desktop.wm.keybindings minimize "['<Super>h']"
+gsettings set org.gnome.desktop.wm.keybindings close "['<Super>w', '<Alt>F4']"
+gsettings set org.gnome.shell.keybindings show-screenshot-ui "['<Shift><Control><Super>4']"
+gsettings set org.gnome.shell.keybindings screenshot "['Print']"
+gsettings set org.gnome.shell.keybindings toggle-message-tray "['<Super>v']"
+gsettings set org.gnome.shell.overrides attach-modal-dialogs false
 
 package_list=(
   gnome-shell-extension-dash-to-panel 
