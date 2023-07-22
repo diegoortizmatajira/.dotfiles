a87ea4ce9f034d03328df743e782a9ee32000427
Diego Ortiz <diegoortizmatajira@gmail.com>
2023-07-22 16:17:16 -0400

    Display control for tv

--- linux/local-bin/display-control.sh
+++ linux/local-bin/display-control.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 EXTERNAL0=DP-0
 EXTERNAL1=DP-2
+EXTERNAL2=HDMI-0
 case $1 in
     'primary-only')
         echo "Leaving only primary display on"
@@ -9,6 +10,10 @@ case $1 in
     'all-external')
         echo "Turning on all external displays"
         gnome-randr --output $EXTERNAL1 --auto --left-of $EXTERNAL0
+        ;;
+    'tv-only')
+        echo "Turning on TV only"
+        gnome-randr --output $EXTERNAL1 --off --output $EXTERNAL0 --off
         ;;
     *)
         echo "Use 'primary-only' or 'all-external'."
--- linux/zsh_os.sh
+++ linux/zsh_os.sh
@@ -1,2 +1,5 @@
 source /usr/share/goto/goto.sh
 export QT_QPA_PLATFORMTHEME=qt5ct
+alias dcp='display-control primary-only'
+alias dca='display-control all-external'
+alias dctv='display-control tv-only'
