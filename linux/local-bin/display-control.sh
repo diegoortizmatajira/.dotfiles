f0d60cd7a57a0566ecca0baa601721a6b72ab9dd
Diego Ortiz <diegoortizmatajira@gmail.com>
2023-11-15 11:47:06 -0500

    Update

--- linux/local-bin/display-control.sh
+++ linux/local-bin/display-control.sh
@@ -5,15 +5,15 @@ EXTERNAL2=HDMI-0
 case $1 in
     'primary-only')
         echo "Leaving only primary display on"
-        gnome-randr --output $EXTERNAL1 --off
+        xrandr --output $EXTERNAL1 --off
         ;;
     'all-external')
         echo "Turning on all external displays"
-        gnome-randr --output $EXTERNAL1 --auto --left-of $EXTERNAL0
+        xrandr --output $EXTERNAL1 --auto --left-of $EXTERNAL0
         ;;
     'tv-only')
         echo "Turning on TV only"
-        gnome-randr --output $EXTERNAL1 --off --output $EXTERNAL0 --off
+        xrandr --output $EXTERNAL1 --off --output $EXTERNAL0 --off
         ;;
     *)
         echo "Use 'primary-only' or 'all-external'."
--- linux/zsh_os.sh
+++ linux/zsh_os.sh
@@ -1,6 +1,7 @@
 source /usr/share/goto/goto.sh
 export QT_QPA_PLATFORMTHEME=qt5ct
 export PATH=/opt/cuda/bin:$PATH
+export PATH=$HOME/.cargo/bin:$PATH
 alias dcp='display-control primary-only'
 alias dca='display-control all-external'
 alias dctv='display-control tv-only'
