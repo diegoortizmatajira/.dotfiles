2df9bdc954ffe4e49309f401dd81efe4806ab0eb
Diego Ortiz <diegoortizmatajira@gmail.com>
2025-06-20 10:32:34 -0400

    MacOS Sync

--- macos/initial_setup.sh
+++ macos/initial_setup.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 #<----------------Installing MacOS Apps, via Homebrew, Cask-------------------->
 brew=(
-    autojump
     cmake
     curl
+    docker
+    docker-buildx
+    docker-completion
+    docker-compose
+    docker-credential-helper
+    eza
+    fastfetch
+    fd
     fzf
     gcc
     git
     git-flow
+    git-lfs
     glib
     go
     grep
@@ -18,53 +26,56 @@ brew=(
     kubernetes-cli
     kubeval
     kustomize
+    lazydocker
     lazygit
     less
-    lsd
     lua
     luajit
     luarocks
     make
-    neofetch
     neovim
     node
+    nvm
+    openjdk
     openssl@1.1
+    openssl@3
     perl
     pyenv
-    python@3.11
-    ranger
+    python@3.13
     ripgrep
+    sevenzip
     speedtest-cli
     sqlite
     the_silver_searcher
     tmux
     tree-sitter
+    virtualenv
     watch
     wget
+    yazi
     zlib
+    zoxide
     zsh-completions
 )
 cask=(
     adobe-acrobat-reader
-    alt-tab
-    amethyst
     barrier
     beyond-compare
     caffeine
     datagrip
+    dotnet-sdk
     font-caskaydia-cove-nerd-font
     font-fira-code-nerd-font
     font-jetbrains-mono-nerd-font
-    git-credential-manager-core
+    ghostty
+    git-credential-manager
     gitkraken
-    hazeover
-    kitty
-    microsoft-office
     rectangle
     sublime-text
+    visual-studio-code
     vlc
-    vscodium
     yt-music
+    zoom
 ) #GUI apps that install with cask
 
 npm=(
