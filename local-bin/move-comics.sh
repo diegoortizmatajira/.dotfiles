938854a252e870a6681b21a80bdd480a89d7964e
Diego Ortiz <diegoortizmatajira@gmail.com>
2025-08-01 12:11:33 -0400

    Move comics from usenet inbox to import

---
+++ local-bin/move-comics.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set your source and destination folders
+SOURCE_DIR="/home/storage/Inbox/Usenet/Comics"
+DEST_DIR="/home/storage/Diego-Ortiz/Lambton-OneDrive/Apps/ComicTrack/content/import"
+
+# Create destination folder if it doesn't exist
+mkdir -p "$DEST_DIR"
+
+# Find and move .cbr and .cbz files
+find "$SOURCE_DIR" -type f \( -iname "*.cbr" -o -iname "*.cbz" \) -exec mv {} "$DEST_DIR" \;
+
+echo "All .cbr and .cbz files have been moved to $DEST_DIR"
